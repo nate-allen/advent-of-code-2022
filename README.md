849e335f2a9a99c03eb94620eb1f42aaafe02b3f
Nate Allen <nate.allen@automattic.com>
2022-12-01 10:48:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Advent of Code 2022 Solutions
+
+My solutions to the [Advent of Code](https://adventofcode.com) puzzles for 2022.
+
+You can run the solutions in terminal like this:
+
+`php -r "require 'day-1.php'; part_1();"`
+
+Change `day-1.php` to the day, and `part_1()` to the part you want to run.
---
+++ day-1.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Finds the Elf with the most calories and the total that elf is carrying.
+ *
+ * @return void
+ */
+function part_1() {
+	// Keep track of who has the most calories.
+	$most_calories = 0;
+
+	// Loop through each elf and calculate their calories.
+	foreach ( get_elves() as $elf ) {
+		// Get the items by new line.
+		$items = explode( "\n", $elf );
+
+		$calories = array_sum( $items );
+
+		// If this elf has more calories than the most calories, update the most calories.
+		if ( $calories > $most_calories ) {
+			$most_calories = $calories;
+		}
+	}
+
+	echo $most_calories . PHP_EOL;
+}
+
+/**
+ * Finds the top 3 elves with the most calories and the total that they are carrying.
+ *
+ * @return void
+ */
+function part_2() {
+	$elf_calories = array();
+
+	// Loop through each elf and calculate their calories.
+	foreach ( get_elves() as $elf ) {
+		// Get the items by new line.
+		$items = explode( "\n", $elf );
+
+		// Use array_sum to get total calories.
+		$elf_calories[] = array_sum( $items );
+	}
+
+	// Sort the elves by calories.
+	sort( $elf_calories );
+
+	// Get the total of the top 3 elves.
+	echo array_sum( array_slice( $elf_calories, -3 ) ) . PHP_EOL;
+}
+
+/**
+ * Returns an array of elves and their calories.
+ *
+ * It gets a list of calories from the data file, and then splits them by blank lines to get each elf.
+ *
+ * @return array An array of elves with their calories.
+ */
+function get_elves() {
+	// Get calories from file.
+	$calories = trim( file_get_contents( __DIR__ . '/data/day-1.txt' ) );
+
+	// Split by blank lines.
+	return preg_split( '/\R\R/', $calories );
+}
