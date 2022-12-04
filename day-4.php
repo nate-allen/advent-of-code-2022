b8b41c414876f087d5e4184d5c303c6fe1253778
Nate Allen <nate.allen@automattic.com>
2022-12-04 01:02:35 -0500

    Add Day 4 solutions

---
+++ day-4.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Day 4: Camp Cleanup
+ */
+
+/**
+ * Part 1: In how many assignment pairs does one range fully contain the other?
+ *
+ * @return void
+ */
+function part_1() {
+	$assignments = get_assignments();
+	$total = 0;
+
+	foreach ( $assignments as $assignment ) {
+		$elf1 = explode( '-', explode( ',', $assignment )[0] );
+		$elf2 = explode( '-', explode( ',', $assignment )[1] );
+
+		if (
+			$elf1[0] >= $elf2[0] && $elf1[1] <= $elf2[1] ||
+			$elf2[0] >= $elf1[0] && $elf2[1] <= $elf1[1]
+		) {
+			$total++;
+		}
+	}
+
+	echo $total . PHP_EOL;
+}
+
+/**
+ * Part 2: In how many assignment pairs do the ranges overlap?
+ *
+ * @return void
+ */
+function part_2() {
+	$assignments = get_assignments();
+	$total = 0;
+
+	foreach ( $assignments as $assignment ) {
+		$elf1 = explode( '-', explode( ',', $assignment )[0] );
+		$elf2 = explode( '-', explode( ',', $assignment )[1] );
+
+		if ( $elf1[1] >= $elf2[0] && $elf1[0] <= $elf2[1] ) {
+			$total++;
+		}
+	}
+
+	echo $total . PHP_EOL;
+}
+
+/**
+ * Returns an array of random strings representing assignment areas.
+ *
+ * @return array
+ */
+function get_assignments() {
+	$data = trim( file_get_contents( __DIR__ . '/data/day-4.txt' ) );
+
+	return explode( "\n", $data );
+}
