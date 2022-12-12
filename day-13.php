5d9b58f23cf7a8000bfd8cac10d6553ee34469ad
Nate Allen <nate.allen@automattic.com>
2022-12-12 16:54:35 -0500

    Add Day 13 template

---
+++ day-13.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Day 13:
+ */
+
+/**
+ * Part 1:
+ *
+ * @return void
+ */
+function part_1() {
+	$lines = get_lines();
+	$total = 0;
+
+	foreach ( $lines as $line ) {
+		$total++;
+	}
+
+	printf( 'Total: %s', $total . PHP_EOL );
+}
+
+/**
+ * Part 2:
+ *
+ * @return void
+ */
+function part_2() {
+	$lines = get_lines();
+	$total = 0;
+
+	foreach ( $lines as $line ) {
+		$total++;
+	}
+
+	printf( 'Total: %s', $total . PHP_EOL );
+}
+
+/**
+ * Returns an array of data from the input file.
+ *
+ * @return array
+ */
+function get_lines() {
+	$line  = file_get_contents( __DIR__ . '/data/day-13.txt' );
+
+	return explode( "\n", $line );
+}
